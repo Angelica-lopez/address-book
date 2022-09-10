729f58031e52f3666ae3de4752999a486329f3a6
Angelica-lopez <angielopez4004@gmail.com>
2022-09-10 17:02:11 -0500

    Update, create and delete clients

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,109 +1,115 @@
-import { useState, useEffect} from "react";
-import {Table, Button} from "react-bootstrap";
-import ClientModal from "../ClientModal/ClientModal";
+import { useState, useEffect } from "react";
 import { getClients } from "../../api";
 import {
-    getClientsFromLocalStorage,
-    updateClientsFromLocalStorage,
+  getClientsFromLocalStorage,
+  updateClientsFromLocalStorage,
 } from "../../utils/localStorage";
+import ClientModal from "../ClientModal";
+import Table from "react-bootstrap/Table";
+import Button from "react-bootstrap/Button";
 
 const App = () => {
-    const [isLoading, setIsLoading] = useState(true);
-    const [clients, setClients] = useState(getClientsFromLocalStorage());
-    const [selectedClient, setSelectedClient] = useState(null);
-    const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [clients, setClients] = useState(getClientsFromLocalStorage());
+  const [selectedClient, setSelectedClient] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-    useEffect(() => {
-        getClients()
-          .then((res) => {
-            console.log(res)
-            if (!clients.length) setClients(res);
-          })
-          .finally(() => setIsLoading(false));
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-      }, []);
-    
-    useEffect(() => {
-        updateClientsFromLocalStorage(clients);
-    }, [clients]);
+  useEffect(() => {
+    getClients()
+      .then((res) => {
+        if (!clients.length) setClients(res);
+      })
+      .finally(() => setIsLoading(false));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-    const openModal = () => setIsModalOpen(true);
+  useEffect(() => {
+    updateClientsFromLocalStorage(clients);
+  }, [clients]);
 
-    const onRemoveClient = (clientId) => {
-        const updatedClients = clients.filter(({ id }) => id !== clientId);
-        setClients(updatedClients);
-      };
+  useEffect(() => {
+    if (!isModalOpen) setSelectedClient(null);
+  }, [isModalOpen]);
 
-    if (isLoading) {
-        return (
-        <div className="p-4 d-flex align-items-center justify-content-center vh-100">
-            <p>Loading...</p>
-        </div>
-        );
-    }
+  const openModal = () => setIsModalOpen(true);
 
+  const onRemoveClient = (clientId) => {
+    const updatedClients = clients.filter(({ id }) => id !== clientId);
+    setClients(updatedClients);
+  };
+
+  if (isLoading)
     return (
-        <div className="p-4">
-            <div className="d-flex justify-content-between align-items-center mb-4">
-                <h1>Clientes</h1>
-                <Button variant='primary' onClick={openModal}>
-                    Crear Cliente
-                </Button>
-            </div>
-            {!clients.length ? (
-                <p>Aun no tienes clientes registrados.</p>
-            ) : (
-                <Table striped bordered>
-                    <thead>
-                        <tr>
-                            <th scope="col">Nombres</th>
-                            <th scope="col">Apellidos</th>
-                            <th scope="col">Dirección</th>
-                            <th scope="col">Telefono</th>
-                            <th scope="col">Acciones</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {clients.map((client) => (
-                            <tr key={client.id}>
-                                <td>{client.nombre}</td>
-                                <td>{client.apellido}</td>
-                                <td>{client.direccion}</td>
-                                <td>{client.telefono}</td>
-                                <td>
-                                    <div className="d-flex">
-                                    <Button
-                                        onClick={() => {
-                                        setSelectedClient(client);
-                                        openModal(true);
-                                        }}
-                                        variant="outline-primary"
-                                        size="sm"
-                                        className="me-2"
-                                    >
-                                        Editar
-                                    </Button>
-                                    <Button
-                                        variant="outline-danger"
-                                        size="sm"
-                                        onClick={() => onRemoveClient(client.id)}
-                                    >
-                                        Eliminar
-                                    </Button>
-                                    </div>
-                                </td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </Table>
-            )}
-            <ClientModal 
-                isModalOpen={isModalOpen}
-                onCloseModal={() => setIsModalOpen(false)}
-            />
-        </div>
-        
-    )
-}
+      <div className="p-4 d-flex align-items-center justify-content-center vh-100">
+        <p>Loading...</p>
+      </div>
+    );
 
-export default App
+  return (
+    <>
+      <div className="p-4">
+        <div className="d-flex justify-content-between align-items-center mb-4">
+          <h1>Clientes</h1>
+          <Button variant="primary" onClick={openModal}>
+            Crear Cliente
+          </Button>
+        </div>
+        {!clients.length ? (
+          <p>Aun no tienes clientes registrados.</p>
+        ) : (
+          <Table striped bordered>
+            <thead>
+              <tr>
+                <th scope="col">Nombres</th>
+                <th scope="col">Apellidos</th>
+                <th scope="col">Dirección</th>
+                <th scope="col">Telefono</th>
+                <th scope="col">Acciones</th>
+              </tr>
+            </thead>
+            <tbody>
+              {clients.map((client) => (
+                <tr key={client.id}>
+                  <td>{client.nombre}</td>
+                  <td>{client.apellido}</td>
+                  <td>{client.direccion}</td>
+                  <td>{client.telefono}</td>
+                  <td>
+                    <div className="d-flex">
+                      <Button
+                        onClick={() => {
+                          setSelectedClient(client);
+                          openModal(true);
+                        }}
+                        variant="outline-primary"
+                        size="sm"
+                        className="me-2"
+                      >
+                        Editar
+                      </Button>
+                      <Button
+                        variant="outline-danger"
+                        size="sm"
+                        onClick={() => onRemoveClient(client.id)}
+                      >
+                        Eliminar
+                      </Button>
+                    </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        )}
+      </div>
+      <ClientModal
+        selectedClient={selectedClient}
+        setClients={setClients}
+        isModalOpen={isModalOpen}
+        onCloseModal={() => setIsModalOpen(false)}
+      />
+    </>
+  );
+};
+
+export default App;
--- src/components/ClientModal/ClientModal.jsx
+++ src/components/ClientModal/ClientModal.jsx
@@ -1,68 +1,184 @@
-import { useState, useEffect} from "react";
-import {Form, Button, Modal, ModalFooter, ModalHeader} from "react-bootstrap";
-import PropTypes from "prop-types"; 
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import uuid from "react-uuid";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
+import { formatDataToEnglish, formatDataToSpanish } from "../../utils/utils";
 
+const defaultFormDataState = {
+  firstName: "",
+  lastName: "",
+  address: "",
+  phoneNumber: "",
+};
 
+const errorsDefaultState = {
+  firstName: false,
+  lastName: false,
+  address: false,
+  phoneNumber: false,
+};
 
-const ClientModal = ({ isModalOpen, onCloseModal }) => {
-    return (
-        <Modal show={isModalOpen} onHide={onCloseModal}>
-            <Modal.Header closeButton>
-                <Modal.Title>
-                    Crear Cliente
-                </Modal.Title>
-            </Modal.Header>  
-                <Form>
-                <Modal.Body>
-                    <div className="row mb-3">
-                        <div className="col">
-                            <Form.Group className="mb-3" controlId="firstName">
-                                <Form.Label>Nombres</Form.Label>
-                                <Form.Control
-                                    type="text"
-                                />
-                            </Form.Group>
-                        </div>
-                        <div className="col">
-                            <Form.Group className="mb-3" controlId="lastName">
-                                <Form.Label>Apellidos</Form.Label>
-                                <Form.Control
-                                    type="text"
-                                />
-                            </Form.Group>
-                        </div>
-                        </div>
-                        <div className="row">
-                        <div className="col">
-                            <Form.Group className="mb-3" controlId="address">
-                                <Form.Label>Dirección</Form.Label>
-                                <Form.Control
-                                    type="text"
-                                />
-                            </Form.Group>
-                        </div>
-                        <div className="col">
-                            <Form.Group className="mb-3" controlId="phoneNumber">
-                                <Form.Label>Teléfono</Form.Label>
-                                <Form.Control
-                                    type="text"
-                                />
-                            </Form.Group>
-                        </div>
-                    </div>
-                </Modal.Body>
-                <ModalFooter>
-                    <Button variant="secondary" type="button" >Cerrar</Button>
-                    <Button variant="primary" type="submit" >Crear</Button>
-                </ModalFooter>
-                </Form>
-        </Modal>
-    )
-}
+const ClientModal = ({
+  selectedClient,
+  setClients,
+  isModalOpen,
+  onCloseModal,
+}) => {
+  const [formData, setFormData] = useState(defaultFormDataState);
+  const [errorsState, setErrorsState] = useState(errorsDefaultState);
+
+  const { firstName, lastName, address, phoneNumber } = formData;
+
+  useEffect(() => {
+    const newFormData = selectedClient
+      ? formatDataToEnglish(selectedClient)
+      : defaultFormDataState;
+    setFormData(newFormData);
+  }, [selectedClient]);
+
+  useEffect(() => {
+    if (!isModalOpen) {
+      setFormData(defaultFormDataState);
+      setErrorsState(errorsDefaultState);
+    }
+  }, [isModalOpen]);
+
+  const onUpdateClient = (updatedClient) => {
+    setClients((prevClients) =>
+      prevClients.map((client) => {
+        if (client.id === selectedClient.id) {
+          return { ...updatedClient, id: selectedClient.id };
+        }
+        return client;
+      })
+    );
+  };
+
+  const onCreateClient = (newClient) => {
+    setClients((prevClients) => [...prevClients, { ...newClient, id: uuid() }]);
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if ([!firstName, !lastName, !address, !phoneNumber].includes(true)) {
+      setErrorsState({
+        firstName: !firstName,
+        lastName: !lastName,
+        address: !address,
+        phoneNumber: !phoneNumber,
+      });
+      return;
+    }
+
+    const formattedData = formatDataToSpanish(formData);
+    if (selectedClient) {
+      onUpdateClient(formattedData);
+    } else {
+      onCreateClient(formattedData);
+    }
+    onCloseModal();
+    setFormData(defaultFormDataState);
+  };
+
+  const onChange = ({ target: { value, id } }) => {
+    setFormData((prevData) => ({
+      ...prevData,
+      [id]: value,
+    }));
+    setErrorsState((prevData) => ({
+      ...prevData,
+      [id]: !value,
+    }));
+  };
+
+  return (
+    <Modal show={isModalOpen} onHide={onCloseModal}>
+      <Modal.Header closeButton>
+        <Modal.Title>
+          {selectedClient ? "Actualizar Cliente" : "Crear Cliente"}
+        </Modal.Title>
+      </Modal.Header>
+      <Form onSubmit={onSubmit}>
+        <Modal.Body>
+          <div className="row mb-3">
+            <div className="col">
+              <Form.Group className="mb-3" controlId="firstName">
+                <Form.Label>Nombres</Form.Label>
+                <Form.Control
+                  type="text"
+                  onChange={onChange}
+                  value={firstName}
+                  isInvalid={errorsState.firstName}
+                />
+              </Form.Group>
+            </div>
+            <div className="col">
+              <Form.Group className="mb-3" controlId="lastName">
+                <Form.Label>Apellidos</Form.Label>
+                <Form.Control
+                  type="text"
+                  onChange={onChange}
+                  value={lastName}
+                  isInvalid={errorsState.lastName}
+                />
+              </Form.Group>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col">
+              <Form.Group className="mb-3" controlId="address">
+                <Form.Label>Dirección</Form.Label>
+                <Form.Control
+                  type="text"
+                  onChange={onChange}
+                  value={address}
+                  isInvalid={errorsState.address}
+                />
+              </Form.Group>
+            </div>
+            <div className="col">
+              <Form.Group className="mb-3" controlId="phoneNumber">
+                <Form.Label>Teléfono</Form.Label>
+                <Form.Control
+                  type="text"
+                  onChange={onChange}
+                  value={phoneNumber}
+                  isInvalid={errorsState.phoneNumber}
+                />
+              </Form.Group>
+            </div>
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" type="button" onClick={onCloseModal}>
+            Cerrar
+          </Button>
+          <Button variant="primary" type="submit">
+            {selectedClient ? "Actualizar" : "Crear"}
+          </Button>
+        </Modal.Footer>
+      </Form>
+    </Modal>
+  );
+};
 
 ClientModal.propTypes = {
-    isModalOpen: PropTypes.bool.isRequired,
-    onCloseModal: PropTypes.func.isRequired,
-}
+  selectedClient: PropTypes.shape({
+    id: PropTypes.string,
+    nombre: PropTypes.string,
+    apellido: PropTypes.string,
+    direccion: PropTypes.string,
+    telefono: PropTypes.string,
+  }),
+  setClients: PropTypes.func.isRequired,
+  isModalOpen: PropTypes.bool.isRequired,
+  onCloseModal: PropTypes.func.isRequired,
+};
+
+ClientModal.defaultProps = {
+  selectedClient: null,
+};
 
 export default ClientModal;
